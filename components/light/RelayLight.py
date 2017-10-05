7c3dc3e17ce1900e41f2fd4a485ba625afb58c38
root <root@wim.lan>
2017-10-05 10:10:55 +0000

    First commit

---
+++ components/light/RelayLight.py
@@ -0,0 +1,77 @@
+import logging
+
+import voluptuous as vol
+
+# Import the device class from the component that you want to support
+from homeassistant.components.light import ATTR_BRIGHTNESS, Light, PLATFORM_SCHEMA
+from homeassistant.const import CONF_HOST, CONF_PORT
+import homeassistant.helpers.config_validation as cv
+
+# Home Assistant depends on 3rd party packages for API specific code.
+REQUIREMENTS = ['lanrelay==0.8']
+
+_LOGGER = logging.getLogger(__name__)
+
+# Validation of the user's configuration
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_HOST): cv.string,
+})
+
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    """Setup the Awesome Light platform."""
+    import lanrelay.lanrelay as ar
+
+    # Assign configuration variables. The configuration check takes care they are
+    # present.
+    # host = config.get(CONF_HOST)
+    # port = config.get(CONF_PORT)
+    host = "192.168.1.166"
+    port = 1234
+    nrelay = 8
+
+    rl = ar.EightChanRelay(host, port, nrelay)
+
+    # Verify that passed in configuration works
+    # if not rl.is_valid_login():
+    #    _LOGGER.error("Could not connect to relay")
+    #    return False
+
+    # Add devices
+    add_devices(RelayLight(relay) for relay in rl.relays)
+
+
+
+class RelayLight(Light):
+    """Representation of a relay light"""
+
+    def __init__(self, light):
+        """Initialize a relay light."""
+        self._light = light
+        self._name = light.name
+        self._state = None
+
+    @property
+    def name(self):
+        """Return the display name of this light."""
+        return self._name
+
+    @property
+    def is_on(self):
+        """Return true if light is on."""
+        return self._state
+
+    def turn_on(self, **kwargs):
+        """Instruct the light to turn on."""
+        self._light.turnOn()
+
+    def turn_off(self, **kwargs):
+        """Instruct the light to turn off."""
+        self._light.turnOff()
+
+    def update(self):
+        """Fetch new state data for this light.
+
+        This is the only method that should fetch new data for Home Assistant.
+        """
+        self._state =  self._light.getStatus()
